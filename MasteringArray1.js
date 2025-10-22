5fe5490b84ddb6b764da8d57e0c5e99df1b8f74a
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-10-22 14:36:54 +0530

    pattern programming and mastering different array operation s

---
+++ MasteringArray1.js
@@ -0,0 +1,132 @@
+// let arr = [];
+// arr.push(100),
+// arr.push(10),
+// arr.push("anmol"),
+// arr.push(false),
+
+
+// console.log(arr[0])
+// console.log(arr);
+
+
+// arr[0]=100
+// arr[1]=20
+// arr[2]=6
+// arr[7]=96
+// arr.pop()
+// console.log(arr);
+
+
+// let a = 20
+
+// console.log(a);
+
+// Javscript is dynamic Programming Language
+
+
+
+// let arr = new Array(3)
+
+// arr[0]=100
+// arr[1]=20
+// arr[2]=6
+// arr[7]=96
+
+// console.log(arr);
+
+
+
+// let arr = []
+// arr[0]=Number(prompt("enter a value"));
+// arr[1]=Number(prompt("enter a value"));
+// arr[2]=Number(prompt("enter a value"));
+// arr[3]=Number(prompt("enter a value"));
+// arr[4]=Number(prompt("enter a value"));
+
+// console.log(arr)
+
+
+// let arr = new Array(5);
+
+// for (let i = 0;i<arr.length;i++){
+//     arr[i]=Number(prompt("enter a value"));
+// }
+
+// console.log(arr)
+
+
+// let arr = [10,20,30,40,50]
+// let sum = 0;
+// for (let i = 0; i<arr.length;i++){
+//     sum = sum +arr[i];
+// }
+
+// console.log(sum);
+
+
+// let arr = [10,2,78,100,450];
+// let max = arr[0];
+// for (let i =1;i<arr.length;i++){
+//     if(max<arr[i]){
+//         max = arr[i]
+//     }
+// }
+
+// console.log(max)
+
+
+// let arr = [10,30,56,43,29,64,49,60];
+// let max = Math.max(arr[0],arr[1]); console.log(max)
+// let sMax = Math.min(arr[0],arr[1]); console.log(sMax)
+
+// for (let i = 2;i<arr.length;i++){
+//     if(max<arr[i]){
+//         max = arr[i]
+//     } else if (sMax<arr[i] && arr[i]<max){
+//         sMax=arr[i]
+//     }
+// }
+
+// console.log(`max is ${max} and sMax is ${sMax}`);
+
+
+// let arr = [10,20,30,40,50,60]
+// let temp = [];
+// let j =0
+// for (let i=arr.length-1;i>=0;i--){
+//     temp[j]=arr[i]
+//     j++
+// }
+// console.log(temp)
+
+
+// let arr = [10,20,30,40,50,60]
+
+// let i = 0, j= arr.length-1;
+
+// while(i<j){
+//     let temp = arr[i]
+//     arr[i]=arr[j]
+//     arr[j]=temp
+//     i++
+//     j--
+// }
+
+// console.log(arr)
+
+
+// let arr = [1,1,0,1,0,1,1,0,0];
+
+// let i =0,j=0;
+
+// while(i<arr.length){
+//     if(arr[i]==0){
+//         let temp = arr[i]
+//         arr[i]=arr[j]
+//         arr[j]=temp
+//         j++
+//     }
+//     i++
+// }
+
+// console.log(arr)
---
+++ PatternProgramming.js
@@ -0,0 +1,113 @@
+// process.stdout.write("hello  ")
+// process.stdout.write("world")
+
+
+// let prompt =  require ('prompt-sync')();
+// let a = prompt("Enter a number")
+// process.stdout.write(a)
+
+// process.stdout.write('*')
+// process.stdout.write('*')
+// process.stdout.write('*')
+// process.stdout.write('*')
+// process.stdout.write('*')
+// process.stdout.write('*')
+
+// let prompt =  require ('prompt-sync')();
+// let n = prompt("Enter a number")
+// for (let i = 1;i<n;i++){process.stdout.write('*')}
+// console.log()
+// for (let i = 1;i<n;i++){process.stdout.write('*')}
+// console.log()
+// for (let i = 1;i<n;i++){process.stdout.write('*')}
+// console.log()
+// for (let i = 1;i<n;i++){process.stdout.write('*')}
+// console.log()
+// for (let i = 1;i<n;i++){process.stdout.write('*')}
+// console.log()
+// for (let i = 1;i<n;i++){process.stdout.write('*')}
+// console.log()
+
+
+
+
+// let prompt =  require ('prompt-sync')();
+// let n = prompt("Enter a number")
+// for (let i = 1;i<n;i++){process.stdout.write('*')}
+// console.log()
+
+
+// let prompt =  require ('prompt-sync')();
+// let n = prompt("Enter a number")
+// for (let i = 1;i<=n;i++){
+//     for (let j = 1;j<=n;j++){
+//     process.stdout.write('*');
+    
+// }console.log()}
+
+// let prompt =  require ('prompt-sync')();
+// let n = prompt("Enter a number :")
+// for (let i = 1;i<=n;i++){
+//     for (let j = 1;j<=i;j++){
+//     process.stdout.write('*');
+     
+// }console.log()}
+
+
+// let prompt =  require ('prompt-sync')();
+// let n = prompt("Enter a number :")
+// for (let i = 1;i<=n;i++)
+//     {  let ch = 65 ;
+  
+//     for (let j = 1;j<=i;j++){
+//     process.stdout.write(String.fromCharCode(ch)+" ");
+//     ch++
+    
+     
+// }console.log()}
+
+
+// let prompt =  require ('prompt-sync')();
+// let n = prompt("Enter a number :")
+// for (let i = 1;i<=n;i++){
+//   for (let j = 1;j<=n-i+1;j++){
+//     process.stdout.write("*");
+//     }console.log()}
+
+
+// let prompt =  require ('prompt-sync')();
+// let n = prompt("Enter a number :")
+// for (let i = 1;i<=n;i++){
+//   for (let j = 1;j<=n-i+1;j++){
+//     process.stdout.write(" ");
+
+//     }
+//     for (let j = 1;j<=i;j++){
+//         process.stdout.write("*");}
+//     console.log()}
+
+
+// let prompt =  require ('prompt-sync')();
+// let n = Number(prompt("Enter a number"))
+// for (let i = 1;i<=n;i++){
+// for (let j = 1;j<=n;j++){
+// if(i ==j || i+j==n+1)
+// {process.stdout.write("*");}
+// else{
+//     process.stdout.write(" ");
+// }
+// }
+// console.log()}
+
+
+// let prompt =  require ('prompt-sync')();
+// let n = Number(prompt("Enter a number"))
+// for (let i = 1;i<=n;i++){
+// for (let j = 1;j<=2*n-1;j++){
+// if(i ==j || i+j==2*n)
+// {process.stdout.write("*");}
+// else{
+//     process.stdout.write(" ");
+// }
+// }
+// console.log()}
